22f85e9b938226a99ef420fdc702e5c58bf1e19f
Alex Loddengaard <alexloddengaard@gmail.com>
2012-10-22 15:26:20 -0700

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surfalert::Application.config.secret_token = '6904bb71646755ee1bece1c9d9d0fcd78cf78c9290f297c1d12d5663ba224ae4038a204ad7a9aaf5c64934fd80f9311b2972ba128309329097ba8d05ca6a3eaf'
