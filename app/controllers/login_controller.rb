6e7839431c5be56775da54d6b1c86feaedb28aab
Alex Loddengaard <alexloddengaard@gmail.com>
2012-11-09 10:20:15 -0800

    Forcing users to login for certain actions.
    Also adding some helpers for getting the current user and seeing
    if the user is logged in or not.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,6 @@
 class LoginController < ApplicationController
+  before_filter :requires_login, :only => [:destroy]
+  
   def create
     user = User.find_by_email(params[:email])  
     if user && user.authenticate(params[:password])  
